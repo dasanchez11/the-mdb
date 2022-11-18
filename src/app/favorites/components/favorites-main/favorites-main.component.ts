739b4fbedb967373a2645e419ce1ee029415319e
JBRIZO <jlaux@applaudostudios.dev>
2022-11-18 12:25:34 -0600

    Start of favorites

--- src/app/favorites/components/favorites-main/favorites-main.component.ts
+++ src/app/favorites/components/favorites-main/favorites-main.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCurrentUser } from 'src/app/auth/store/auth.selectors';
+import { User } from 'src/app/auth/interfaces/responses/get-account-response';
+import { Observable } from 'rxjs'
+import { AppState } from 'src/app/app.store';
+
 
 @Component({
   selector: 'app-favorites-main',
   templateUrl: './favorites-main.component.html',
 })
 export class FavoritesMainComponent implements OnInit {
-  constructor() {}
+  userLoggedIn$! : Observable<User | null>
+  constructor(private store : Store<AppState>) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userLoggedIn$ = this.store.select(selectCurrentUser)
+  }
 }
