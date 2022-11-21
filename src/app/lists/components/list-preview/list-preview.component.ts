3af5ddbbff1c9657bd0fe7445542811988fa394a
JBRIZO <jlaux@applaudostudios.dev>
2022-11-21 11:22:31 -0600

    Removed delete list feature
    Removed delete list functionality because of API error

--- src/app/lists/components/list-preview/list-preview.component.ts
+++ src/app/lists/components/list-preview/list-preview.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Store } from '@ngrx/store';
-import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 import { IListDetails } from '../../interfaces/list-details-response.interface';
-import { deleteList } from '../../store/lists.actions';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-preview',
@@ -13,26 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class ListPreviewComponent {
   @Input() list!: IListDetails;
-  confirmDialog!: MatDialogRef<ConfirmationDialogComponent>;
 
-  deleteVisible = false;
+  constructor( ) {}
 
-  constructor(private dialog: MatDialog, private store: Store) {}
-
-  openConfirmationDialog(): Observable<boolean> {
-    this.confirmDialog = this.dialog.open(ConfirmationDialogComponent, {
-      disableClose: true,
-    });
-    this.confirmDialog.componentInstance.dialogMessage =
-      'Are you sure you want to delete this list?';
-    return this.confirmDialog.afterClosed();
-  }
-
-  deleteList(): void {
-    this.openConfirmationDialog().subscribe(result => {
-      if (result) {
-        this.store.dispatch(deleteList({ listId: parseInt(this.list.id!) }));
-      }
-    });
-  }
 }
