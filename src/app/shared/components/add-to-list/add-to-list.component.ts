c8e989bb44c3cec438bd3e9823735e2183982cd9
JBRIZO <jlaux@applaudostudios.dev>
2022-11-22 19:03:46 -0600

    Added movie exists in a list validation

--- src/app/shared/components/add-to-list/add-to-list.component.ts
+++ src/app/shared/components/add-to-list/add-to-list.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { AppState } from 'src/app/app.store';
 import { IListDetails } from 'src/app/lists/interfaces/list-details-response.interface';
-import { selectAllLists } from 'src/app/lists/store/lists.selector';
-import { Observable } from 'rxjs'
 import { addMovieToList } from 'src/app/lists/store/lists.actions';
-import { Movie } from 'src/app/home/interfaces/movies.interface';
+import { selectAllLists } from 'src/app/lists/store/lists.selector';
 
 @Component({
   selector: 'app-add-to-list',
@@ -24,6 +23,14 @@ export class AddToListComponent implements OnInit {
 
   addMovieToList(listId : string) {
     this.store.dispatch(addMovieToList({movieId: this.movieId, listId: parseInt(listId)}))
+  }
+
+  isMovieInList(list : IListDetails) : boolean {
+    if(list){
+      const movie = list.items.find(movie => movie.id === this.movieId)
+      return movie !== undefined
+    }
+    return false;
   }
 
 }
