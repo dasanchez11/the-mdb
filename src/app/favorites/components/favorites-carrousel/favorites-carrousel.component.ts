14c1c016a263fff658ba6b998c7bdac683c177e9
JBRIZO <jlaux@applaudostudios.dev>
2022-11-23 12:52:46 -0600

    Fixed favorites module based on feedback

--- src/app/favorites/components/favorites-carrousel/favorites-carrousel.component.ts
+++ src/app/favorites/components/favorites-carrousel/favorites-carrousel.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subject, takeUntil } from 'rxjs';
 import { AppState } from 'src/app/app.store';
 import { Movie } from 'src/app/home/interfaces/movies.interface';
 import { loadFavorites } from '../../store/favorites.actions';
@@ -9,19 +10,22 @@ import { selectFavoriteMovies } from '../../store/favorites.selectors';
   selector: 'app-favorites-carrousel',
   templateUrl: './favorites-carrousel.component.html',
 })
-export class FavoritesCarrouselComponent implements OnInit {
+export class FavoritesCarrouselComponent implements OnInit, OnDestroy {
   movies!: Movie[];
   page!: number;
   totalPages!: number;
+  private unsubscribe = new Subject<void>() 
 
   constructor(private store: Store<AppState>) {}
-
+  
   ngOnInit(): void {
-    this.store.select(selectFavoriteMovies).subscribe(favorites => {
+    this.store.select(selectFavoriteMovies).pipe(
+      takeUntil(this.unsubscribe)
+    ).subscribe(favorites => {
       this.movies = favorites.results;
       this.page = favorites.page;
       this.totalPages = favorites.total_pages;
-    });
+    })
   }
 
   onScroll() {
@@ -29,4 +33,11 @@ export class FavoritesCarrouselComponent implements OnInit {
       this.store.dispatch(loadFavorites({ page: ++this.page }));
     }
   }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next()
+    this.unsubscribe.complete()
+  }
+
+
 }
